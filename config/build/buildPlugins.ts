7119ec66e85d08559b8a086fe5ff59043d7580d1
Kant <kant@mail.ru>
2023-02-14 22:42:51 +0300

    add webpack bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -20,5 +21,8 @@ export function buildPlugins({
             __IS_DEV__: JSON.stringify(isDev),
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ];
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import App from './app/App';
 import './shared/config/i18n/i18n';
 
