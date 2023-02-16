99e5b311d42358f19966e4d2d2c20bf51e325d69
Kant <kant@mail.ru>
2023-02-16 19:47:31 +0300

    pal1000

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -20,10 +20,6 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
 
     if (isDev) {
