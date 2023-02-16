846b5c7504912c49e98b36bb27cf4c192cc7ce12
Kant <kant@mail.ru>
2023-02-16 15:00:06 +0300

    add loki and docker

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -7,16 +7,6 @@
 import path from 'path';
 
 export default {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\асхпаб\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls, instances and results before every test
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
