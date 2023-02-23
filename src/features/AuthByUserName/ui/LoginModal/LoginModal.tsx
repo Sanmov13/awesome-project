424b2bcf62f3abb659a93239e52060752536b086
Kant <kant@mail.ru>
2023-02-23 23:20:17 +0300

    add input, change modal

--- src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUserName/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
 className?: string;
@@ -11,7 +10,7 @@ onClose: () => void
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
     <Modal
-        className={classNames(cls.LoginModal, {}, [className])}
+        className={classNames('', {}, [className])}
         isOpen={isOpen}
         onClose={onClose}
         lazy
