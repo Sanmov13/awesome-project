cf7ecd7285042fb34774a26fd85dbee49b265386
Kant <movsank@gmail.com>
2023-03-31 11:53:57 +0300

    add fixes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -8,6 +8,7 @@ import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => {
     const isAuth = useSelector(getUserAuthData);
+
     const routes = useMemo(() => Object.values(routeConfig).filter((route) => {
         if (route.authOnly && !isAuth) {
             return false;
